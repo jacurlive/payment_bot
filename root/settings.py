feab73d9db8dfcf0ce59caf65e001925d7876e46
jacurlive <jasurkkirchh@gmail.com>
2026-01-31 18:07:41 +0500

    update

--- root/settings.py
+++ root/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASS"),
+        "HOST": "127.0.0.1",
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
